89bcd36ffef7226bf35502d6a1d2d368eb67f764
riron1206 <rironyokoi@gmail.com>
2021-05-04 04:59:09 +0900

    add py from my_PC

--- app.py
+++ app.py
@@ -38,11 +38,13 @@ def load_df(d_dir, name):
 
 
 @st.cache(allow_output_mutation=True)
-def get_data(st_index, col):
+def get_stock_data(st_index):
     # 株価指標のデータロード
-    df_index = load_df(index_dir, st_index)
-    df_index = df_index[["日付", col]].rename(columns={col: st_index})
+    return load_df(index_dir, st_index).sort_values(by="日付").reset_index(drop=True)
 
+
+@st.cache(allow_output_mutation=True)
+def get_stock_col_data(col):
     # 個別銘柄のデータロード
     stock_ids = [Path(csv).stem for csv in glob.glob(stock_dir + "/*csv")]
     df_stocks = None
@@ -53,51 +55,84 @@ def get_data(st_index, col):
             df_stocks = df
         else:
             df_stocks = pd.merge(df_stocks, df, on="日付", how="outer")
+    return df_stocks.sort_values(by="日付").reset_index(drop=True)
+
+
+def get_merge_data(st_index, col):
+    # 株価指標のデータロード
+    df_index = get_stock_data(st_index)
+    df_index = df_index[["日付", col]].rename(columns={col: st_index})
+
+    # 個別銘柄のデータロード
+    df_stocks = get_stock_col_data(col)
 
     # 株価指標と個別銘柄マージ
     df_merge = pd.merge(df_index, df_stocks, on="日付", how="outer")
 
-    return df_merge
+    return df_merge.sort_values(by="日付").reset_index(drop=True)
 
 
 def main():
-    st.markdown("# Start dateを基準にした株価の増加率")
-
     # サイドバー
     st_index = st.sidebar.selectbox("比較基準の株価指標", ("nikkei225", "jpx400", "topix"))
     st_p_type = st.sidebar.selectbox("価格の種類", ("始値", "終値", "高値", "安値"))
-
-    # 株価指標と個別銘柄まとめたデータフレーム
-    df_merge = get_data(st_index, st_p_type)
-
-    # サイドバー
     st_start_date = st.sidebar.date_input(
         "Start date", datetime.datetime.strptime("2019-1-1", "%Y-%m-%d").date()
     )
     st_end_date = st.sidebar.date_input("End date", datetime.date.today())
-    st_n_limit = st.sidebar.slider("表示する銘柄の件数", 1, 100, step=1, value=10)
+    st_n_limit = st.sidebar.slider("グラフで表示する銘柄の件数", 1, 100, step=1, value=3)
+    st_corr_limit = st.sidebar.slider("グラフで表示する相関係数の範囲", -1.0, 1.0, (-1.0, 1.0))
+
+    # 株価指標と個別銘柄まとめたデータフレーム
+    df_merge = get_merge_data(st_index, st_p_type)
+    df_stock_kashi = get_stock_col_data("貸株残高")
+    df_stock_deki = get_stock_col_data("出来高")
 
     # 期間絞る
     df_merge = df_merge[
         (df_merge["日付"] >= str(st_start_date)) & (df_merge["日付"] <= str(st_end_date))
     ]
+    df_stock_kashi = df_stock_kashi[
+        (df_stock_kashi["日付"] >= str(st_start_date))
+        & (df_stock_kashi["日付"] <= str(st_end_date))
+    ]
+    df_stock_deki = df_stock_deki[
+        (df_stock_deki["日付"] >= str(st_start_date))
+        & (df_stock_deki["日付"] <= str(st_end_date))
+    ]
+
+    # 貸株残高の前日との差分
+    df_stock_kashi_diff = df_stock_kashi.copy()
+    for c in df_stock_kashi_diff.columns[1:]:
+        df_stock_kashi_diff[c] = df_stock_kashi_diff[c].diff()
+
+    # 出来高の前日との差分
+    df_stock_deki_diff = df_stock_deki.copy()
+    for c in df_stock_deki_diff.columns[1:]:
+        df_stock_deki_diff[c] = df_stock_deki_diff[c].diff()
 
     # 基準日からの増加率計算
-    for c in df_merge.columns[1:]:
-        df_merge[c] = (df_merge[c] - df_merge.iloc[-1][c]) / df_merge.iloc[-1][c] + 100
+    df_rate = df_merge.copy()
+    for c in df_rate.columns[1:]:
+        base = df_rate.iloc[0][c]
+        df_rate[c] = (df_rate[c] - base) / base + 100
 
     # 株価指標と個別銘柄の相関係数
-    corr = df_merge.corr()
+    corr = df_rate.corr()
     corr = corr[st_index].sort_values()
-    corr_high = corr[(corr > 0.7) & (corr != 1)][st_n_limit * -1 :]
-    corr_low = corr[(corr < -0.7)][st_n_limit * -1 :]
+    corr_high = corr[
+        (corr > st_corr_limit[0]) & (corr < st_corr_limit[1]) & (corr != 1)
+    ][st_n_limit * -1 :].sort_values(ascending=False)
 
     # plot
-    title = f"{st_index}の{st_p_type}と高い正の相関(>0.7)がある銘柄のみ表示"
+    st.markdown("# Start dateを基準にした株価の増加率")
+    title = (
+        f"{st_index}の{st_p_type}と相関({st_corr_limit[0]}<r<{st_corr_limit[1]})がある銘柄のみ表示"
+    )
     if len(corr_high) > 0:
         st.write(
             px.line(
-                df_merge,
+                df_rate,
                 x="日付",
                 y=[st_index] + corr_high.index.to_list(),
                 title=title,
@@ -108,20 +143,38 @@ def main():
     else:
         st.write(title + "なし")
 
-    title = f"{st_index}の{st_p_type}と高い負の相関(<-0.7)がある銘柄のみ表示"
-    if len(corr_low) > 0:
-        st.write(
-            px.line(
-                df_merge,
-                x="日付",
-                y=[st_index] + corr_low.index.to_list(),
-                title=title,
-                height=800,
-                width=800,
-            )
+    st.write("\n\n")
+    st.write("### 株価の増加率の相関係数一覧")
+    st.dataframe(
+        pd.DataFrame(corr.sort_values(ascending=False)),
+        height=500,
+        width=800,
+    )
+
+    # plot
+    st.write("\n")
+    st.markdown("# 貸株残高の前日との差分")
+    st.write(
+        px.line(
+            df_stock_kashi_diff,
+            x="日付",
+            y=corr_high.index.to_list(),
+            height=800,
+            width=800,
         )
-    else:
-        st.write(title + "なし")
+    )
+
+    # plot
+    st.markdown("# 出来高の前日との差分")
+    st.write(
+        px.line(
+            df_stock_deki_diff,
+            x="日付",
+            y=corr_high.index.to_list(),
+            height=800,
+            width=800,
+        )
+    )
 
 
 if __name__ == "__main__":
